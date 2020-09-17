a0b1e425adf7cd6e0b4a8ebf312e96fff681569c
Riley Deraes <RileyDeraes@iCloud.com>
2020-09-17 16:07:57 -0400

    Done with 4 and 5, taking a little break.

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ class Lambdasian {
     this.age = object.age;
     this.location = object.location;
   }
-  
+
     speak() {
       return `Hello, my name is ${this.name}, and I am from ${this.location}!`
     }
@@ -128,7 +128,7 @@ class Instructor extends Lambdasian {
   }
 
   grade(student, subject) {
-
+    return `${student.name} receives a perfect score on ${subject}`
   }
 }
 
@@ -147,12 +147,28 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor (object) {
+    super(object);
+    this.previousBackground = object.previousBackground;
+    this.className = object.className;
+    this.favSubjects = object.favSubjects;
+  }
 
+  listSubjects() {
+    return this.favSubjects.toString();
+  }
+
+  PRAssignment() {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge() {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
-/*
-  TASK 6
+/* TASK 6
     - Write a ProjectManager class extending Instructor.
     - Its constructor takes a single argument - an object with the following keys:
         + All the keys used to initialize instances of Instructor.
@@ -162,20 +178,10 @@ class Student {
     - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
     - ProjectManager instances have the following methods:
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
-        + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
-*/
+        + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}` */
 class ProjectManager {
 
 }
-
-/*
-  STRETCH PROBLEM (no tests!)
-    - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-    - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-    - Add a graduate method to a student.
-      + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-      + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
-*/
 
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
